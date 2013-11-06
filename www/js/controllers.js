8c58cfcaa941c6d88b8ad2f627a7cc6647b2b868
unknown <maryum.babar@KL-03.kualitatem.local>
2013-11-06 15:07:58 +0500

    6-setPOS4/launch external4 6

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2333,6 +2333,86 @@ function SweetCtrl($window, UpdateService, $log, $scope, sweetService, interacti
                     
                     
                     
+                     map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions); 
+                    
+                    
+                    
+                    //--------------------------------------------------------------------------------------
+            //------------------------------ AutoComplete Box --------------------------------------------
+            //--------------------------------------------------------------------------------------
+            var input = (document.getElementById('target'));
+            var autocomplete = new google.maps.places.Autocomplete(input);
+
+            autocomplete.bindTo('bounds', map);
+
+            var infowindow = new google.maps.InfoWindow();
+            var marker = new google.maps.Marker({
+                map:map
+            });
+            
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                infowindow.close();
+                marker.setVisible(false);
+                //input.className = '';
+                var place = autocomplete.getPlace();
+                if (!place.geometry) {
+                    // Inform the user that the place was not found and return.
+                    //input.className = 'notfound';
+                    return;
+                }
+
+                // If the place has a geometry, then present it on a map.
+                //if (place.geometry.viewport) {
+                //console.log("place.geometry.viewport");
+                //map.fitBounds(place.geometry.viewport);
+                //} else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);
+
+                console.log("LatLong" + place.geometry.location);
+                //console.log(" -->" + _.pairs(place.photos[0]));
+                console.log(" -->" + place.reference);
+                console.log(" -->" + place.rating);
+                console.log(" -->" + place.name);
+                console.log(" -->" + place.icon);
+                console.log(" -->" + place.formatted_address);
+                console.log(" -->");
+
+                $rootScope.placeSearchResults.LatLong = place.geometry.location;
+                //$rootScope.placeSearchResults.photo = place.photos;
+                $rootScope.placeSearchResults.gname = place.name;
+                $rootScope.placeSearchResults.icon = place.icon;
+                $rootScope.placeSearchResults.formatted_address = place.formatted_address;
+                console.log("placeSearchResults -->" + $rootScope.placeSearchResults.gname);
+
+                $scope.placeClaim($rootScope.placeSearchResults);
+
+                //}
+                marker.setIcon(/** @type {google.maps.Icon} **/ ({
+                    url:place.icon,
+                    size:new google.maps.Size(71, 71),
+                    origin:new google.maps.Point(0, 0),
+                    anchor:new google.maps.Point(17, 34),
+                    scaledSize:new google.maps.Size(35, 35)
+                }));
+                marker.setPosition(place.geometry.location);
+                marker.setVisible(true);
+
+                var address = '';
+                if (place.address_components) {
+                    address = [
+                        (place.address_components[0] && place.address_components[0].short_name || ''),
+                        (place.address_components[1] && place.address_components[1].short_name || ''),
+                        (place.address_components[2] && place.address_components[2].short_name || '')
+                    ].join(' ');
+                }
+
+                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+                infowindow.open(map, marker);
+            });
+
+            //--------------------------------------------------------------------------------------
+            //--------------------------------------------------------------------------------------
                     /*var infowindow = new google.maps.InfoWindow({
                      map: map,
                      position: pos,
@@ -2359,7 +2439,7 @@ function SweetCtrl($window, UpdateService, $log, $scope, sweetService, interacti
                 handleNoGeolocation(false);
             }
             
-            map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+          
             
             var defaultBounds = new google.maps.LatLngBounds(
                 new google.maps.LatLng(-33.8902, 151.1759),
@@ -2442,82 +2522,7 @@ function SweetCtrl($window, UpdateService, $log, $scope, sweetService, interacti
             //--------------------------------------------------------------------------------------
             //--------------------------------------------------------------------------------------
 
-            //--------------------------------------------------------------------------------------
-            //------------------------------ AutoComplete Box --------------------------------------------
-            //--------------------------------------------------------------------------------------
-            var input = (document.getElementById('target'));
-            var autocomplete = new google.maps.places.Autocomplete(input);
-
-            autocomplete.bindTo('bounds', map);
-
-            var infowindow = new google.maps.InfoWindow();
-            var marker = new google.maps.Marker({
-                map:map
-            });
             
-            google.maps.event.addListener(autocomplete, 'place_changed', function () {
-                infowindow.close();
-                marker.setVisible(false);
-                //input.className = '';
-                var place = autocomplete.getPlace();
-                if (!place.geometry) {
-                    // Inform the user that the place was not found and return.
-                    //input.className = 'notfound';
-                    return;
-                }
-
-                // If the place has a geometry, then present it on a map.
-                //if (place.geometry.viewport) {
-                //console.log("place.geometry.viewport");
-                //map.fitBounds(place.geometry.viewport);
-                //} else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(17);
-
-                console.log("LatLong" + place.geometry.location);
-                //console.log(" -->" + _.pairs(place.photos[0]));
-                console.log(" -->" + place.reference);
-                console.log(" -->" + place.rating);
-                console.log(" -->" + place.name);
-                console.log(" -->" + place.icon);
-                console.log(" -->" + place.formatted_address);
-                console.log(" -->");
-
-                $rootScope.placeSearchResults.LatLong = place.geometry.location;
-                //$rootScope.placeSearchResults.photo = place.photos;
-                $rootScope.placeSearchResults.gname = place.name;
-                $rootScope.placeSearchResults.icon = place.icon;
-                $rootScope.placeSearchResults.formatted_address = place.formatted_address;
-                console.log("placeSearchResults -->" + $rootScope.placeSearchResults.gname);
-
-                $scope.placeClaim($rootScope.placeSearchResults);
-
-                //}
-                marker.setIcon(/** @type {google.maps.Icon} **/ ({
-                    url:place.icon,
-                    size:new google.maps.Size(71, 71),
-                    origin:new google.maps.Point(0, 0),
-                    anchor:new google.maps.Point(17, 34),
-                    scaledSize:new google.maps.Size(35, 35)
-                }));
-                marker.setPosition(place.geometry.location);
-                marker.setVisible(true);
-
-                var address = '';
-                if (place.address_components) {
-                    address = [
-                        (place.address_components[0] && place.address_components[0].short_name || ''),
-                        (place.address_components[1] && place.address_components[1].short_name || ''),
-                        (place.address_components[2] && place.address_components[2].short_name || '')
-                    ].join(' ');
-                }
-
-                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-                infowindow.open(map, marker);
-            });
-
-            //--------------------------------------------------------------------------------------
-            //--------------------------------------------------------------------------------------
             //add marker on double click
             ////////////////////////////////////////////////////////////////
             var markersArray = [];
@@ -2758,7 +2763,7 @@ function SweetCtrl($window, UpdateService, $log, $scope, sweetService, interacti
            // var address = ($rootScope.placeSearchResults.gname).replace(" ", "");
             //console.log('----> ' + address);
             userService.logout();
-            window.open('http://m.facebook.com');
+            window.open('http://www.jklabz.com/sweetnessblue/#/lahore2');
             /*$scope.safeApply(function () {
                 $location.path($rootScope.placeSearchResults.gname);
             });*/
