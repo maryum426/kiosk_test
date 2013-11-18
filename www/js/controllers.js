1a7a63b063afc47bbc56988269c1e3cf13645d3a
unknown <maryum.babar@KL-03.kualitatem.local>
2013-11-18 11:51:22 +0500

    3-updatedb1 18

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5309,8 +5309,8 @@ function CameraCtrl($window, UpdateService, $log, $scope, sweetService, interact
 
                 //alert("Source Set!");
 
-                var parseAPPID = "h2w6h5BLXG3rak7sQ2eyEiTKRgu3UPzQcjRzIFCu";
-                var parseJSID = "gQ7DmgLGTDNNl4Nl9l3cmJkSluy4y2hEPVaNSH2k";
+                var parseAPPID = "WRdpguLGfYdPVMq2LwHiB0s5k9ESVTwdde7kXwDm";
+                var parseJSID = "MzJ2jpG740oPfRdsKRY6jbXHCeEDXwTlnVFUYiTi";
 
                 //Initialize Parse
                 Parse.initialize(parseAPPID,parseJSID);
